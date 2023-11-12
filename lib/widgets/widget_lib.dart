c5c9babf2bf00920e636ad269f998d7e59116ce6
ImpulZzZ <lukas.drothler@web.de>
2023-11-12 21:11:09 +0100

    new parts

--- lib/widgets/widget_lib.dart
+++ lib/widgets/widget_lib.dart
@@ -3,5 +3,6 @@ library widget_lib;
 import 'package:flutter/material.dart';
 import 'package:meta_insights_tft_frontend/models/composition_group.dart';
 
-part 'trait_styles_row.dart';
+part 'combination_row.dart';
 part 'composition_group_table.dart';
+part 'composition_view.dart';
