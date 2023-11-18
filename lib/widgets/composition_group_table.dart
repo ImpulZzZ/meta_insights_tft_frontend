33a5a8337dbe3ad57afb2487a0f1d2c1b0dcd5ed
ImpulZzZ <lukas.drothler@web.de>
2023-11-18 12:51:29 +0100

    nullable fields

--- lib/widgets/composition_group_table.dart
+++ lib/widgets/composition_group_table.dart
@@ -1,8 +1,8 @@
 part of widget_lib;
 
 class CompositionGroupTable extends StatefulWidget {
-  List<CompositionGroup> compositionGroups;
-  Map<String, String> icons;
+  List<CompositionGroup>? compositionGroups;
+  Map<String, String>? icons;
   String groupBy;
 
   CompositionGroupTable(
@@ -18,8 +18,8 @@ class _CompositionGroupTableState extends State<CompositionGroupTable> {
   @override
   Widget build(BuildContext context) {
     return CompositionView(
-        compositionGroups: widget.compositionGroups,
-        icons: widget.icons,
+        compositionGroups: widget.compositionGroups ?? [],
+        icons: widget.icons ?? {},
         groupBy: widget.groupBy);
   }
 }
