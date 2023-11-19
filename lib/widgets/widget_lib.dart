e8258fefc1bd01df6bce76ff86528eb09ce011ee
ImpulZzZ <lukas.drothler@web.de>
2023-11-19 17:24:45 +0100

    removed unnessesary widget

--- lib/widgets/widget_lib.dart
+++ lib/widgets/widget_lib.dart
@@ -4,5 +4,4 @@ import 'package:flutter/material.dart';
 import 'package:meta_insights_tft_frontend/models/composition_group.dart';
 
 part 'combination_row.dart';
-part 'composition_group_table.dart';
 part 'composition_view.dart';
