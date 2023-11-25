94c6945fa5ebbd1a9abfad08b1bb0505661c504f
ImpulZzZ <lukas.drothler@web.de>
2023-11-25 22:11:42 +0100

    added stars for champions and texts

--- lib/widgets/combination_row.dart
+++ lib/widgets/combination_row.dart
@@ -6,38 +6,67 @@ class CombinationRow extends StatelessWidget {
   final String groupBy;
 
   const CombinationRow(
-      {super.key, required this.composition, required this.icons, required this.groupBy});
+      {super.key,
+      required this.composition,
+      required this.icons,
+      required this.groupBy});
 
   Color getStyleColor(int? style) {
     switch (style) {
+      case 0:
+        return const Color.fromARGB(255, 255, 255, 255);
       case 1:
         return const Color.fromARGB(255, 167, 116, 98);
       case 2:
         return const Color.fromARGB(255, 132, 164, 180);
       case 3:
         return const Color.fromARGB(255, 194, 180, 58);
-      case 4:
+      default:
         return const Color.fromARGB(255, 102, 225, 241);
     }
-    return const Color.fromARGB(255, 255, 255, 255);
+  }
+
+  Row getStarRow(int? starAmount) {
+    List<Icon> stars = [];
+    for (var i = 0; i < starAmount!; i++) {
+      stars.add(const Icon(
+        Icons.star,
+        color: Color.fromARGB(255, 255, 115, 0),
+        size: 10,
+      ));
+    }
+    return Row(children: stars);
   }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        for (var current in composition.keys)
-          Container(
-            width: 30,
-            height: 30,
-            color:
-                groupBy == "trait" ? getStyleColor(composition[current]) : null,
-            child: Image(
-              image: AssetImage('assets${icons[current]}'),
-              fit: BoxFit.fill,
-              color: groupBy == "trait" ? Colors.black : null,
-            ),
-          )
+        for (var current in composition.keys) ...[
+          Column(
+            children: [
+              Text(current,
+                  style: const TextStyle(
+                      fontSize: 10, fontWeight: FontWeight.bold)),
+              groupBy == "champion"
+                  ? getStarRow(composition[current])
+                  : Container(),
+              Container(
+                width: 65,
+                height: 65,
+                color: groupBy == "trait"
+                    ? getStyleColor(composition[current])
+                    : null,
+                child: Image(
+                  image: AssetImage('assets${icons[current]}'),
+                  fit: BoxFit.fill,
+                  color: groupBy == "trait" ? Colors.black : null,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(width: 10),
+        ]
       ],
     );
   }
